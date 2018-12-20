2e273f4296006d91df85f570a43f9c8fe9b9fa25
jin <jinyuli91@gmail.com>
2018-12-20 16:35:29 -0500

    day 0 data type completed

---
+++ 10daysJS/dataType.js
@@ -0,0 +1,44 @@
+// Objective
+//
+// Today, we're discussing data types. Check out the attached tutorial for more details.
+//
+// Task
+//
+// Variables named , , and  are declared for you in the editor below. You must use the  operator to perform the following sequence of operations:
+//
+// Convert  to an integer (Number type), then sum it with  and print the result on a new line using console.log.
+// Convert  to a floating-point number (Number type), then sum it with  and print the result on a new line using console.log.
+// Print the concatenation of  and  on a new line using console.log. Note that must be printed first.
+// Input Format
+//
+// Data Type	Parameter	Description
+// string		The string representation of an integer you must sum with .
+// string		The string representation of a floating-point number you must sum with .
+// string		A string of one or more space-separated words you must append to .
+// Output Format
+//
+// Print the following three lines of output:
+//
+// On the first line, print the sum of  and the integer representation of .
+// On the second line, print the sum of  and the floating-point representation of .
+// On the third line, print  concatenated with . You must print  before .
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank ';
+
+    // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+    console.log(firstInteger + parseInt(secondInteger));
+
+    // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+    console.log(firstDecimal + parseFloat(secondDecimal));
+
+    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+    console.log(firstString + secondString)
+}
