726d193c8bec29b0faa8ed84e977fed81fe0cb7a
jin <jinyuli91@gmail.com>
2018-12-29 15:26:58 -0500

    finished let and const

---
+++ 10daysJS/letAndConst.js
@@ -0,0 +1,49 @@
+// Declare a constant variable, , and assign it the value Math.PI. You will not pass this challenge unless the variable is declared as a constant and named PI (uppercase).
+// Read a number, , denoting the radius of a circle from stdin.
+// Use  and  to calculate the  and  of a circle having radius .
+// Print  as the first line of output and print  as the second line of output.
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    let r = readLine();
+    const PI = Math.PI;
+
+    // Print the area of the circle:
+    console.log(PI * r * r);
+
+    // Print the perimeter of the circle:
+    console.log(2 * PI * r)
+
+
+    try {
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
